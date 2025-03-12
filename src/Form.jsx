9dc5f740145d7784cc55f554461c43e6060f4f95
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-03-12 16:58:53 +0100

    Modify Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -21,9 +21,12 @@ import { BsFillCheckSquareFill } from 'react-icons/bs'
 
 export const Form = () => {
   const methods = useForm()
+  const [success, setSuccess] = useState(false)
 
   const onSubmit = methods.handleSubmit(data => {
     console.log(data)
+    methods.reset()
+    setSuccess(true)
   })
 
   return (
@@ -42,6 +45,11 @@ export const Form = () => {
           <Input {...desc_validation} className="md:col-span-2" />
         </div>
         <div className="mt-5">
+          {success && (
+            <p className="flex items-center gap-1 mb-5 font-semibold text-green-500">
+              <BsFillCheckSquareFill /> Form has been submitted successfully
+            </p>
+          )}
           <button
             onClick={onSubmit}
             className="flex items-center gap-1 p-5 font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-800"
