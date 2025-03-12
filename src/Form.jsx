6b236d6cd79a52c5b070b0670c8b6f1c53108f91
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-03-12 16:47:32 +0100

    Modify Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -26,6 +26,42 @@ export const Form = () => {
     console.log(data)
   })
 
+  const name_validation = {
+    name: 'name',
+    label: 'name',
+    type: 'text',
+    id: 'name',
+    placeholder: 'write your name ...',
+    validation: {
+      required: {
+        value: true,
+        message: 'required',
+      },
+      maxLength: {
+        value: 30,
+        message: '30 characters max',
+      },
+    },
+  }
+
+  const password_validation = {
+    name: 'password',
+    label: 'password',
+    type: 'password',
+    id: 'password',
+    placeholder: 'type password ...',
+    validation: {
+      required: {
+        value: true,
+        message: 'required',
+      },
+      minLength: {
+        value: 6,
+        message: 'min 6 characters',
+      },
+    },
+  }
+
   return (
     <FormProvider {...methods}>
       <form
@@ -35,36 +71,8 @@ export const Form = () => {
         className="container"
       >
         <div className="grid gap-5 md:grid-cols-2">
-          <Input
-            label="name"
-            name="name"
-            type="text"
-            id="name"
-            placeholder="type your name..."
-            validation={{
-              required: {
-                value: true,
-                message: 'required',
-              },
-            }}
-          />
-          <Input
-            label="password"
-            name="password"
-            type="password"
-            id="password"
-            placeholder="type your password..."
-            validation={{
-              required: {
-                value: true,
-                message: 'required',
-              },
-              minLength: {
-                value: 6,
-                message: 'min 6 characters',
-              },
-            }}
-          />
+          <Input {...name_validation} />
+          <Input {...password_validation} />
         </div>
         <div className="mt-5">
           <button
