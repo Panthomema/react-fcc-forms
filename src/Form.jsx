16936220bcdd5b1ccb56a46294733f65a92bf08a
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-03-12 13:58:55 +0100

    Modify Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -28,19 +28,42 @@ export const Form = () => {
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={e => e.preventDefault()} noValidate className="container">
+      <form
+        onSubmit={e => e.preventDefault()}
+        noValidate
+        autoComplete="off"
+        className="container"
+      >
         <div className="grid gap-5 md:grid-cols-2">
           <Input
             label="name"
+            name="name"
             type="text"
             id="name"
             placeholder="type your name..."
+            validation={{
+              required: {
+                value: true,
+                message: 'required',
+              },
+            }}
           />
           <Input
             label="password"
+            name="password"
             type="password"
             id="password"
             placeholder="type your password..."
+            validation={{
+              required: {
+                value: true,
+                message: 'required',
+              },
+              minLength: {
+                value: 6,
+                message: 'min 6 characters',
+              },
+            }}
           />
         </div>
         <div className="mt-5">
