014e3af694253f7aba4bb4cf6af8da4611e6f332
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-03-12 16:50:39 +0100

    Modify Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -26,42 +26,6 @@ export const Form = () => {
     console.log(data)
   })
 
-  const name_validation = {
-    name: 'name',
-    label: 'name',
-    type: 'text',
-    id: 'name',
-    placeholder: 'write your name ...',
-    validation: {
-      required: {
-        value: true,
-        message: 'required',
-      },
-      maxLength: {
-        value: 30,
-        message: '30 characters max',
-      },
-    },
-  }
-
-  const password_validation = {
-    name: 'password',
-    label: 'password',
-    type: 'password',
-    id: 'password',
-    placeholder: 'type password ...',
-    validation: {
-      required: {
-        value: true,
-        message: 'required',
-      },
-      minLength: {
-        value: 6,
-        message: 'min 6 characters',
-      },
-    },
-  }
-
   return (
     <FormProvider {...methods}>
       <form
@@ -72,6 +36,8 @@ export const Form = () => {
       >
         <div className="grid gap-5 md:grid-cols-2">
           <Input {...name_validation} />
+          <Input {...email_validation} />
+          <Input {...num_validation} />
           <Input {...password_validation} />
         </div>
         <div className="mt-5">
